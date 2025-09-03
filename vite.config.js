7559bc342067b3ab03761eba05165ccd3dbd26a3
Walker Lockard <walker.b.lockard@gmail.com>
2025-09-03 13:13:02 -0500

    remove route mw

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { routeMiddleware } from './src/preprocessors/middleware/index.ts';
 
 export default defineConfig({
-  plugins: [tailwindcss(), routeMiddleware(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()]
 });
