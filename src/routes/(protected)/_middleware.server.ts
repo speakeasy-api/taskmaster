c484acdbeb449a18d27d893c4bbea8d6452c006f
Walker Lockard <walker.b.lockard@gmail.com>
2025-09-03 13:10:54 -0500

    delete mw

--- src/routes/(protected)/_middleware.server.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestEvent } from './$types';
-
-export async function middleware(event: RequestEvent) {
-  // Example: Check authentication
-  console.log('Validating session for routes...');
-  await event.locals.validateSession();
-
-  return;
-}
--- src/routes/(protected)/projects/project/[project_id]/_middleware.server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestEvent } from './$types';
-
-export async function middleware(event: RequestEvent) {
-  console.log('Checking if you have access to project:', event.params.project_id);
-
-  // Return nothing/undefined to continue to the route handler
-  return;
-}
